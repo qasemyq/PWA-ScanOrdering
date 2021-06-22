18c4f29acfa9c12e9e2e2285a093a73fa0f7d209
Ghader Ghasemi <68222268+qasemyq@users.noreply.github.com>
2021-06-22 21:56:55 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
+//
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
